7923ac6dfed52f2f1121c7e1f75263cca8c8ff27
Muhammad-Taha-Qader <m.tahaqader000@gmail.com>
2024-07-27 12:59:32 +0500

    Game Board with Players Turns

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -1,28 +1,26 @@
 import React, { useState, useEffect } from 'react';
+let turn=false;
 
 function BoardBox({val}){ //When you want to use a JavaScript expression or a variable inside JSX, you need to wrap it in curly braces.
     //Destructuring Props:In the BoardBox function component, the props are being destructured. val is extracted from the props object. This is similar to how you might destructure an object in JavaScript.
     const[boxVal, SetBoxVal]= useState(' ');
-    console.log('In Box fun: '+val);
     function ExecuteTurn(){
-        if(val){
+        if(turn && boxVal==' '){
             SetBoxVal('X');
-            val= (!val);
-        }else{
+            turn= (!turn);
+        }else if(!turn && boxVal==' '){
             SetBoxVal('O');
-            val= (!val);
+            turn= (!turn);
         }
     }
 
     return <button type='button' onClick={ExecuteTurn} style={{height:17+'px', width:20+'px'}}>{boxVal}</button>
 }
 export function TicTacToe() {
-    let turn=false;
     return (
         <div>
             <div>
-                <BoardBox val={turn} />
-                {console.log('In main fun: '+turn)}
+                <BoardBox />
                 <BoardBox/>
                 <BoardBox/>
             </div>
